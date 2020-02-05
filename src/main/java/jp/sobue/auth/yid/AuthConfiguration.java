8a85552d41b89b13485dd4b0c8263060a8e1c760
Sho Sobue <meru26.sho@gmail.com>
2020-02-05 23:52:15 +0900

    add sample

---
+++ src/main/java/jp/sobue/auth/yid/AuthConfiguration.java
@@ -0,0 +1,23 @@
+package jp.sobue.auth.yid;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class AuthConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    // @formatter:off
+    http.authorizeRequests(
+            a ->
+                a.antMatchers("/", "/top", "/error", "/webjars/**")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated())
+        .oauth2Login()
+        .defaultSuccessUrl("/view");
+    // @formatter:on
+  }
+}
---
+++ src/main/java/jp/sobue/auth/yid/SampleController.java
@@ -0,0 +1,22 @@
+package jp.sobue.auth.yid;
+
+import java.util.Collections;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SampleController {
+
+  @GetMapping({"", "top"})
+  public ModelAndView top() {
+    return new ModelAndView("top");
+  }
+
+  @GetMapping({"view"})
+  public ModelAndView view(@AuthenticationPrincipal OAuth2User principal) {
+    return new ModelAndView("view", Collections.singletonMap("principal", principal));
+  }
+}
